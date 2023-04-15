17376dcb3a358ccebba8f4ac300e1f89dda721fe
Inwerwm <saclainvervm@gmail.com>
2023-04-15 18:58:15 +0900

    リファクタリング

--- PEPDuplicateModel/Duplicater.cs
+++ PEPDuplicateModel/Duplicater.cs
@@ -34,70 +34,9 @@ namespace PEPDuplicateModel
             rootsNode.Name = pmx.RootNode.Items.FirstOrDefault()?.BoneItem?.Bone?.Name ?? "すべての親";
             rootsNode.NameE = pmx.RootNode.Items.FirstOrDefault()?.BoneItem?.Bone?.NameE ?? "Sub Root";
 
-            for (int duplicateNum = 1; duplicateNum < count; duplicateNum++)
+            foreach (var duplicant in Enumerable.Range(1, count - 1).Select(i => CreateDuplicant(pmx, i)))
             {
-                var numSuffix = $" |{duplicateNum}";
-                var offset = MakeOffset(duplicateNum);
-
-                var duplication = (IPXPmx)pmx.Clone();
-
-                foreach (var item in duplication.Body)
-                {
-                    item.Name += numSuffix;
-                    item.NameE += numSuffix;
-                    item.Position += offset;
-                    pmx.Body.Add(item);
-                }
-
-                foreach (var item in duplication.Bone)
-                {
-                    item.Name += numSuffix;
-                    item.NameE += numSuffix;
-                    item.Position += offset;
-                    pmx.Bone.Add(item);
-                }
-
-                foreach (var item in duplication.ExpressionNode.Items)
-                {
-                    pmx.ExpressionNode.Items.Add(item);
-                }
-
-                foreach (var item in duplication.Joint)
-                {
-                    item.Name += numSuffix;
-                    item.NameE += numSuffix;
-                    item.Position += offset;
-                    pmx.Joint.Add(item);
-                }
-
-                foreach (var item in duplication.Material)
-                {
-                    item.Name += numSuffix;
-                    item.NameE += numSuffix;
-                    pmx.Material.Add(item);
-                }
-
-                foreach (var item in duplication.Morph)
-                {
-                    item.Name += numSuffix;
-                    item.NameE += numSuffix;
-                    pmx.Morph.Add(item);
-                }
-
-                foreach (var (item, nodeId) in duplication.Node.Select((node, id) => (node, id)))
-                {
-                    item.Name += numSuffix;
-                    item.NameE += numSuffix;
-                    pmx.Node.Insert(nodeId * duplicateNum + nodeId + duplicateNum, item);
-                }
-
-                foreach (var item in duplication.Vertex)
-                {
-                    item.Position += offset;
-                    pmx.Vertex.Add(item);
-                }
-
-                rootsNode.Items.Add(duplication.RootNode.Items.FirstOrDefault());
+                rootsNode.Items.Add(duplicant.RootNode.Items.FirstOrDefault());
             }
 
             pmx.Node.Insert(0, rootsNode);
@@ -105,9 +44,75 @@ namespace PEPDuplicateModel
             return pmx;
         }
 
-        private static V3 MakeOffset(int num)
+        private static IPXPmx CreateDuplicant(IPXPmx pmx, int duplicationIndex)
         {
-            var quot = Math.DivRem(num - 1, 4, out var directionNum);
+            var duplicant = (IPXPmx)pmx.Clone();
+
+            var numSuffix = $" |{duplicationIndex}";
+            var offset = MakeOffset(duplicationIndex);
+
+            foreach (var item in duplicant.Body)
+            {
+                item.Name += numSuffix;
+                item.NameE += numSuffix;
+                item.Position += offset;
+                pmx.Body.Add(item);
+            }
+
+            foreach (var item in duplicant.Bone)
+            {
+                item.Name += numSuffix;
+                item.NameE += numSuffix;
+                item.Position += offset;
+                pmx.Bone.Add(item);
+            }
+
+            foreach (var item in duplicant.ExpressionNode.Items)
+            {
+                pmx.ExpressionNode.Items.Add(item);
+            }
+
+            foreach (var item in duplicant.Joint)
+            {
+                item.Name += numSuffix;
+                item.NameE += numSuffix;
+                item.Position += offset;
+                pmx.Joint.Add(item);
+            }
+
+            foreach (var item in duplicant.Material)
+            {
+                item.Name += numSuffix;
+                item.NameE += numSuffix;
+                pmx.Material.Add(item);
+            }
+
+            foreach (var item in duplicant.Morph)
+            {
+                item.Name += numSuffix;
+                item.NameE += numSuffix;
+                pmx.Morph.Add(item);
+            }
+
+            foreach (var (item, nodeId) in duplicant.Node.Select((node, id) => (node, id)))
+            {
+                item.Name += numSuffix;
+                item.NameE += numSuffix;
+                pmx.Node.Insert(nodeId * duplicationIndex + nodeId + duplicationIndex, item);
+            }
+
+            foreach (var item in duplicant.Vertex)
+            {
+                item.Position += offset;
+                pmx.Vertex.Add(item);
+            }
+
+            return duplicant;
+        }
+
+        private static V3 MakeOffset(int index)
+        {
+            var quot = Math.DivRem(index - 1, 4, out var directionNum);
             var scale = 5.0f * (quot + 1);
 
             switch (directionNum)
